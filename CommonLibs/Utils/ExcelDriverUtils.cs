f491cb6fdcefd2f206eff046d3ab5ad64ddbafc9
Saurabh Dhingra <saurabh.d2106@gmail.com>
2020-05-16 15:12:02 +0530

    Added Data read Utility

---
+++ CommonLibs/Utils/ExcelDriverUtils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+
+namespace CommonLibs.Utils
+{
+    public class ExcelDriverUtils
+    {
+        public static DataTable ReadDataFromExcel(string filename, string sheetname)
+        {
+            _ = filename.Trim();
+
+            //Read Excel sheet as a stream
+            FileStream stream = File.Open(filename, FileMode.Open, FileAccess.Read);
+
+
+            //Loads the stream in an Excel Reader
+            IExcelDataReader excelReader;
+
+            if (filename.EndsWith(".xls") || filename.EndsWith(".xlsx") || filename.EndsWith(".csv"))
+            {
+                excelReader = ExcelReaderFactory.CreateReader(stream);
+
+            }
+            else
+            {
+                throw new Exception("Invalid File Type");
+            }
+
+
+            //Load the data in the excel reader as Dataset
+            DataSet result = excelReader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+
+            DataTableCollection allTables = result.Tables;
+
+            //Get the data in a DataTable
+            DataTable dataTable = allTables[sheetname];
+
+            return dataTable;
+        }
+    }
+}
